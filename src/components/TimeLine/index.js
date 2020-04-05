92d3933bf829221cc1572990bc39e4815fc05352
Luane Aquino <luanecaroline91@gmail.com>
2020-04-05 17:47:41 -0300

    add timeline

--- src/components/TimeLine/index.js
+++ src/components/TimeLine/index.js
@@ -1,24 +1,47 @@
 import React from "react";
 import { FiHeart } from 'react-icons/fi'
+import axios from 'axios';
+import Helmet from "react-helmet";
 
-export default function TimeLine() {
+export default class TimeLine extends React.Component {
+  state = {
+    conteudo: []
+  }
 
-  return (
-    <div className="post">
-      <header>
-        <img src="https://randomuser.me/api/portraits/women/17.jpg" alt="user" />
-        <div className="post-user">
-          <strong>nasa</strong>
-          <span>Mars</span>
+  componentDidMount = () => {
+    axios.get("http://localhost:3000/posts")
+      .then(res => {
+        this.setState({ conteudo: res.data })
+      })
+  }
+
+  render() {
+    return (
+      <>
+        <Helmet><title>Timeline</title></Helmet>
+        <div className="post">
+          {this.state.conteudo.map(content => (
+            <div key={content.id}>
+              <header>
+                <img src={content.userPicture} alt="Lala" />
+                <div className="post-user">
+                  <strong>{content.user}</strong>
+                  <span>{content.location}</span>
+                </div>
+              </header>
+              <div className="post-image">
+                <img src={content.postPicture} alt="daniela" />
+              </div>
+              <div className="post-likes">
+                <FiHeart />
+              </div>
+              <p>{content.description}</p>
+            </div>
+          ))
+          }
         </div>
-      </header>
-      <div className="post-image">
-        <img src="https://www.publicdomainpictures.net/pictures/90000/velka/mars.jpg" alt="post" />
-      </div>
-      <div className="post-likes">
-        <FiHeart />
-      </div>
-      <p>Waving goodbye to a spacecraft.</p>
-    </div>
-  );
+      </>
+    );
+  }
+
 }
