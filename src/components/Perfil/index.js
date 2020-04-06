f7a9626ecf711b39100a7dce3f18da337c87f77a
Luane Aquino <luanecaroline91@gmail.com>
2020-04-06 17:35:50 -0300

    add basic perfil

--- src/components/Perfil/index.js
+++ src/components/Perfil/index.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React from "react"
+import { Helmet } from 'react-helmet'
 
 export default function Perfil() {
   return (
-    <p>heheheheheheh</p>
+    <>
+      <Helmet><title>Perfil</title></Helmet>
+      <div className="page">
+        <div className="conteudo">
+          <div className="flex">
+            <img src="https://randomuser.me/api/portraits/women/19.jpg" alt="Perfil" />
+
+            <div className="resumo">
+              <h1>Luane</h1>
+              <p>resumo exemplo</p>
+              <h2>Minha Stack</h2>
+              <ul>
+                <li>javascript</li>
+                <li>React</li>
+              </ul>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </>
   );
 }
--- src/pages/perfil/index.js
+++ src/pages/perfil/index.js
@@ -1,11 +1,9 @@
 import React from "react"
-import { Helmet } from 'react-helmet'
 import PerfilComp from '../../components/Perfil'
 
 export default function Perfil() {
   return (
     <>
-      <Helmet><title>Perfil</title></Helmet>
       <PerfilComp />
     </>
   );
