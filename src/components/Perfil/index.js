d5ac592b001e519edcd7217caea4854a68d49307
Luane Aquino <luanecaroline91@gmail.com>
2020-04-06 18:18:11 -0300

    does perfil

--- src/components/Perfil/index.js
+++ src/components/Perfil/index.js
@@ -1,29 +1,53 @@
 import React from "react"
 import { Helmet } from 'react-helmet'
+import axios from 'axios'
 import './styles.css'
 
-export default function Perfil() {
-  return (
-    <>
-      <Helmet><title>Perfil</title></Helmet>
-      <div className="page">
-        <div className="conteudo">
-          <div className="flex">
-            <img src="https://randomuser.me/api/portraits/women/19.jpg" alt="Perfil" />
+export default class Perfil extends React.Component {
+  state = {
+    content: {}
+  }
 
-            <div className="resumo">
-              <h1>Luane</h1>
-              <p>resumo exemplo</p>
-              <h2>Minha Stack</h2>
-              <ul>
-                <li>javascript</li>
-                <li>React</li>
-              </ul>
+  componentDidMount = () => {
+    axios.get("http://localhost:3000/perfil")
+      .then(resposta => {
+        this.setState({ content: resposta.data })
+      })
+  }
+
+  render() {
+    const { content } = this.state
+    return (
+      <>
+        <Helmet><title>Perfil</title></Helmet>
+        <div className="page">
+          <div className="conteudo">
+            <div className="flex">
+              <img src={content.userPicture} alt="Perfil" />
+
+              <div className="resumo">
+                <h1>{content.name}</h1>
+                <br></br>
+                <h4>{content.bio}</h4>
+                <p>{content.age} anos</p>
+                <p>Site: {content.site}</p>
+                <p>Joined at {content.joinAt}</p>
+                <br></br>
+                <h2>Minha Stack</h2>
+                {content.stacks !== undefined && (
+                  <ul>
+                    {content.stacks.map((item, indice) => (
+                      <li key={indice}>{item}</li>
+                    ))}
+                  </ul>
+                )}
+              </div>
             </div>
-          </div>
 
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
+
 }
