634216c80ad389d3424a9d8040be44ce6512c7b9
Luane Aquino <luanecaroline91@gmail.com>
2020-04-07 10:33:12 -0300

    Updates

---
+++ precache-manifest.dc50b437a7c7aec6df857cbb27bc1fa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c02afcbdc182502c563a2c3c2477c9",
+    "url": "/instagram-clone-react-app/index.html"
+  },
+  {
+    "revision": "fe33d50e005838612f18",
+    "url": "/instagram-clone-react-app/static/css/main.1a066f5d.chunk.css"
+  },
+  {
+    "revision": "8a5e7752eeed0c3701d3",
+    "url": "/instagram-clone-react-app/static/js/2.16262f8c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/instagram-clone-react-app/static/js/2.16262f8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe33d50e005838612f18",
+    "url": "/instagram-clone-react-app/static/js/main.fada5d8a.chunk.js"
+  },
+  {
+    "revision": "96fb77d762ddd9ce0b64",
+    "url": "/instagram-clone-react-app/static/js/runtime-main.a023dd63.js"
+  },
+  {
+    "revision": "1ee9adf38a105af291a096c050c96690",
+    "url": "/instagram-clone-react-app/static/media/logo-instagram.1ee9adf3.png"
+  }
+]);
